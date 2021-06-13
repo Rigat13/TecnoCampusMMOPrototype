0239f5f8cb6d48a7c67f9cc7f85815a0172c763e
Carles Rigat Almansa <carlesrigat@gmail.com>
2021-06-13 23:23:36 +0200

    Classes update

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2019.4.18f1\Editor\Unity.exe" -quit -batchmode -projectPath "D:\TECNOCAMPUS\SEGON\LABORATORI DEL SOFTWARE\Prototype" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
