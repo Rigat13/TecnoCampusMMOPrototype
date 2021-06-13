ac06e2e920e7e83e3f659aadf764074ec17b5663
Carles Rigat Almansa <carlesrigat@gmail.com>
2021-06-13 20:44:10 +0200

    Hello world - multiplayer addition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveForward = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * 10, 0, moveForward * Time.deltaTime  * 10);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
